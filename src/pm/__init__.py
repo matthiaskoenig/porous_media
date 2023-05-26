3a6120af45885895be4892a4354486d3d4bbab77
Matthias König <konigmatt@googlemail.com>
2023-05-26 10:36:54 +0200

    MeshResults

--- src/pm/__init__.py
+++ src/pm/__init__.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-BASE_DIR = Path("__file__").parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent
 DATA_DIR = BASE_DIR / "data"
 
-EXAMPLE_VTK = DATA_DIR / "simulation.xlsx"
-EXAMPLE_SIMULATION_XLSX = DATA_DIR / "example.vtk"
+EXAMPLE_VTK = DATA_DIR / "example.vtk"
+EXAMPLE_SIMULATION_XLSX = DATA_DIR / "simulation.xlsx"
 
 
--- src/pm/vtk_example.py
+++ src/pm/vtk_example.py
@@ -1,22 +1,90 @@
 from pathlib import Path
+from typing import Dict, Tuple
 
+import numpy as np
+
+from pm.console import console
 import meshio
 import pyvista as pv
 
-if __name__ == "__main__":
-    vtk_example = Path(__file__).parent.parent.parent / "data" / "sim001_perf1_BC1.t025.vtk"
-    mesh = meshio.read(vtk_example)
-    print(mesh)
-    print("points:", mesh.points)
-    print("cells:", mesh.cells)
+from dataclasses import dataclass, field
 
-    print("data:", mesh.point_data.keys())
-    print("data:", mesh.cell_data.keys())
-    # mesh.points, mesh.cells, ...
 
+@dataclass
+class MeshResults:
+    mesh: meshio.Mesh
+    cell_data: Dict[str, np.ndarray] = field(repr=False)
+    cell_data_shape: Dict[str, Tuple]
+    point_data: Dict[str, np.ndarray] = field(repr=False)
+    point_data_shape: Dict[str, Tuple]
+
+
+def read_mesh_data_from_vtk(vtk_path: Path) -> MeshResults:
+    """Reads mesh information from VTK."""
+    console.rule(align="left", style="white")
+    console.print(vtk_path)
+    mesh: meshio.Mesh = meshio.read(vtk_path)
+    # data can be accessed from the mesh via
+    # print("points:", mesh.points)
+    # print("cells:", mesh.cells)
+    # print("data:", mesh.point_data.keys())
+    # print("data:", mesh.cell_data.keys())
+
+    cell_data: Dict[str, np.ndarray] = {}
+    cell_data_shape: Dict[str, np.ndarray] = {}
+    for key in mesh.cell_data.keys():
+        data = process_cell_data(key=key, mesh=mesh)
+        cell_data[key] = data
+        cell_data_shape[key] = data.shape
+
+    point_data: Dict[str, np.ndarray] = {}
+    point_data_shape: Dict[str, np.ndarray] = {}
+    for key in mesh.point_data.keys():
+        data = process_point_data(key=key, mesh=mesh)
+        point_data[key] = data
+        point_data_shape[key] = data.shape
+
+    # console.print(f"{key: <20}: {type(data)}, {data.shape}")
+
+    mesh_results = MeshResults(
+        mesh=mesh,
+        cell_data=cell_data,
+        cell_data_shape=cell_data_shape,
+        point_data=point_data,
+        point_data_shape=point_data_shape,
+    )
+    console.print(mesh_results)
+    return mesh_results
+
+
+def process_cell_data(key: str, mesh: meshio.Mesh) -> np.ndarray:
+    """Process data from the mesh."""
+    data = mesh.cell_data[key]
+    data = np.dstack(data)
+    return data.squeeze()
+
+
+def process_point_data(key: str, mesh: meshio.Mesh) -> np.ndarray:
+    """Process data from the mesh."""
+    data = mesh.point_data[key]
+    data = np.dstack(data)
+    return data.squeeze()
+
+
+def visualize_mesh_pyvista(mesh: meshio.Mesh) -> None:
+    """Blocking visualization with pyvista."""
     # simply pass the numpy points to the PolyData constructor
     # https://docs.pyvista.org/version/stable/examples/00-load/create-tri-surface.html#sphx-glr-examples-00-load-create-tri-surface-py
     cloud = pv.PolyData(mesh.points)
     cloud.plot(point_size=15)
-    surf = cloud.delaunay_2d()
-    surf.plot(show_edges=True)
+
+
+if __name__ == "__main__":
+    from pm import EXAMPLE_VTK
+    mesh_results: MeshResults = read_mesh_data_from_vtk(EXAMPLE_VTK)
+
+    # visualize_mesh_pyvista(mesh_results.mesh)
+
+
+
+
