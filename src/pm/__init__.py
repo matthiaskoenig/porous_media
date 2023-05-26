73849c3cab236702a90ee75ed201cb0ba6f9099f
Matthias König <konigmatt@googlemail.com>
2023-05-26 09:37:47 +0200

    latest changes

---
+++ src/pm/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_DIR = Path("__file__").parent.parent.parent
+DATA_DIR = BASE_DIR / "data"
+
+EXAMPLE_VTK = DATA_DIR / "simulation.xlsx"
+EXAMPLE_SIMULATION_XLSX = DATA_DIR / "example.vtk"
+
+
