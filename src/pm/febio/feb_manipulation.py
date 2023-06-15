552eda6cdf17a5870c75d166840109307721da1a
Matthias Koenig <konigmatt@googlemail.com>
2023-06-15 11:56:07 +0200

    testing febio

--- src/pm/febio/feb_manipulation.py
+++ src/pm/febio/feb_manipulation.py
@@ -1,10 +1,17 @@
+"""Create febio files.
+
+dependencies:
+    numpy
+    jinja2
+"""
+
+
 from pathlib import Path
 from typing import List, Dict
 import jinja2
 import numpy as np
-import pandas as pd
 
-from pm.console import console
+# from pm.console import console
 
 
 def create_feb_files(feb_template: Path, feb_data: Dict[str, Dict], output_dir: Path):
@@ -36,38 +43,10 @@ def create_feb_files(feb_template: Path, feb_data: Dict[str, Dict], output_dir: 
     for sim_id, context in feb_data.items():
         feb_str = str(template.render(context))
         out_path = output_dir / f"{prefix}_{sim_id}.feb"
-        console.print(f"{out_path}: {context}")
+        # console.print(f"{out_path}: {context}")
+        print(f"{out_path}: {context}")
         with open(out_path, "w") as f_feb:
             f_feb.write(feb_str)
-
-
-def create_feb_files_xlsx(feb_template: Path, sim_xlsx: Path, output_dir: Path, mapping: Dict[str, str]):
-    """Create feb files from the XLSX definition.
-
-    No unit conversion is performed! So the data in excel must be in the right units.
-
-    :param feb_template: path to feb template file with tags
-    :param feb_data: dictionary of simulation_id:context information for rendering (provides information for the tags)
-    :param output_dir: directory where the feb files are generated.
-    :param mapping: mapping of tag:column from excel.
-    """
-    df = pd.read_excel(sim_xlsx)
-    console.print(df)
-
-    feb_data = {}
-    for k, row in df.iterrows():
-        context = {}
-        for tag, column in mapping.items():
-            context[tag] = row[column]
-
-        sim_id = row["simulation_id"]
-        feb_data[sim_id] = context
-
-    create_feb_files(
-        feb_template=feb_template,
-        feb_data=feb_data,
-        output_dir=feb_out_dir,
-    )
 
 
 if __name__ == "__main__":
@@ -77,7 +56,8 @@ if __name__ == "__main__":
     # -----------------------------
     # Example1: simple rendering
     # -----------------------------
-    console.rule(title="Example1", align="left", style="white")
+    # console.rule(title="Example1", align="left", style="white")
+    print("-" * 80)
     feb_out_dir: Path = DATA_DIR / "febio" / "febs_example1"
 
     feb_data = {
@@ -97,26 +77,10 @@ if __name__ == "__main__":
     )
 
     # --------------------------------
-    # Example2: use xlsx information
+    # Example2: use numpy vectors
     # --------------------------------
-    console.rule(title="Example2", align="left", style="white")
-    feb_template = DATA_DIR / "febio" / "lobule_BCflux.feb.template"
-    feb_out_dir: Path = DATA_DIR / "febio" / "scanpf_xlsx"
-    xlsx_path: Path = DATA_DIR / "febio" / "scan_pressure_flow.xlsx"
-    create_feb_files_xlsx(
-        feb_template=feb_template,
-        sim_xlsx=xlsx_path,
-        output_dir=feb_out_dir,
-        mapping={
-            "boundary_pressure": "pressure_pf",
-            "boundary_flux": "substrate_flow"
-        }
-    )
-
-    # --------------------------------
-    # Example3: use numpy vectors
-    # --------------------------------
-    console.rule(title="Example3", align="left", style="white")
+    # console.rule(title="Example2", align="left", style="white")
+    print("-" * 80)
     boundary_pressure = np.linspace(start=1.37*133, stop=5*133, num=10)
     boundary_flow = np.linspace(start=0.00008441679, stop=0.00000422083951, num=10)
 
