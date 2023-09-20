290b26369d3da97bbf1b43a1c85537ce203e9e41
Matthias Koenig <konigmatt@googlemail.com>
2023-09-20 09:36:28 +0200

    working on dataframe calculation

---
+++ docs/ICCB2023_meeting_notes.md
@@ -0,0 +1,20 @@
+# Agent-based modelling 
+Ralph Müller - Aging and osteoporosis
+- Lifespan vs health span; life span increased, but health span not; i.e. ~60 years
+
+bone forming and architecture remodeling
+- bone forming cells (osteoblasts)
+- bone resorbing cells (osteocytes)
+- mechanosensitive cells
+
+interplay is called "remodeling"
+-> allows adaptation to new load condition
+-> renewal of old bone materials
+
+- bone loss attributed to the decrease in mechanical usage
+- mechanical loading results in enhanced bone formation
+=> prevention of bone fractures
+
+Failure mechanisms at the tissue level; concavity and convexity are important
+
+- high resolution micro-CT; loadinng changes; 
--- src/porous_media/data/xdmf_calculations.py
+++ src/porous_media/data/xdmf_calculations.py
@@ -9,10 +9,88 @@ Provide some simple helpers for common operations, e.g.
 
 This should work on cell_data and point_data of the mesh
 
+A lot of the information should be position dependent for analysis.
+Support selections of positions; histogramm over position.
+
 """
+from pathlib import Path
+from typing import Tuple, Dict
+
+import meshio
+import numpy as np
+import pandas as pd
+from rich.progress import track
+
+from porous_media import DATA_DIR
 from porous_media.console import console
+from porous_media.data.xdmf_tools import XDMFInformation
+
+
+def create_mesh_dataframes(xdmf_path: Path) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Create the cell_data and point_data DataFrame.
+
+    Here the actual mesh geometry does not matter any more, but we integrate over all the elements.
+
+    depending on the timepoints this will be a pretty large dataframe.
+
+    """
+    df_cells = None
+    df_points = None
+
+    data_cells: Dict[str, np.ndarray] = {}
+    data_points: Dict[str, np.ndarray] = {}
+
+    with meshio.xdmf.TimeSeriesReader(xdmf_path) as reader:
+        points, cells = reader.read_points_cells()
+
+        tnum = reader.num_steps
+        for k in track(range(tnum), description=f"Creating DataFrames ..."):
+            t, point_data, cell_data = reader.read_data(k)
+
+            # parse cell data
+            for key, data in cell_data.items():
+                console.print(key)
+                data = np.dstack(data)
+                data = data.squeeze()
+                console.print(data.shape)
+                cell_data[key] = data
+
+
+            # parse point data
+            for key, data in point_data.items():
+                console.print(key)
+                data = np.dstack(data)
+                data = data.squeeze()
+                console.print(data.shape)
+                point_data[key] = data
+
+            # FIXME: handle the timecourse aspect in the dataframe
+            # FIXME: use xarray with timecourse dimension to handle the time dimension
+            if k == 0:
+                break
+
+    # generate dataframes
+    # TODO
+    df_cells = pd.DataFrame(data_cells)
+    df_points = pd.DataFrame(data_points)
+
+    return df_cells, df_points
+
+
+
+
+
 
 if __name__ == "__main__":
     console.rule(title="XDMF calculations", style="white")
 
     # FIXME: use the example of one of the zonation patterns with 10 timepoints
+    xdmf_path = DATA_DIR / "spt" / "spt_zonation_patterns_new" / "simulation_pattern0.xdmf"
+    xdmf_info: XDMFInformation = XDMFInformation.from_path(xdmf_path)
+    console.print(xdmf_info)
+
+    df_cells, df_points = create_mesh_dataframes(xdmf_path)
+    console.rule(align="left", title="Cell data", style="white")
+    console.print(df_cells)
+    console.rule(align="left", title="point data", style="white")
+    console.print(df_points)
