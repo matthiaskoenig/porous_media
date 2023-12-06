8a5e32d057318563be0eaa251e81c3df116a4bad
Matthias König <konigmatt@googlemail.com>
2023-12-06 16:47:18 +0100

    cleanup processing script

--- src/porous_media/analyses/spt/spt_data_processing.py
+++ src/porous_media/analyses/spt/spt_data_processing.py
@@ -1,32 +1,23 @@
 """Process all SPT simulations."""
 
 from pathlib import Path
-from typing import Dict, Iterable, List, Set
-
-import numpy as np
-
+from typing import Dict
 
 from porous_media.console import console
 from porous_media.data.xdmf_calculations import mesh_datasets_from_xdmf
 from porous_media.data.xdmf_tools import (
-    DataLimits,
     XDMFInfo,
-    interpolate_xdmf,
     xdmfs_from_directory,
 )
-from porous_media.log import get_logger
 
 
-logger = get_logger(__name__)
-
-
-if __name__ == "__main__":
-
+def process_spt_simulations(input_dir: Path, output_dir: Path) -> None:
+    """Process SPT simulation results."""
     # process files
-    xdmf_dir = Path("/home/mkoenig/git/porous_media/data/spt/2023-12-05")
+
     xdmf_paths: Dict[Path, Path] = xdmfs_from_directory(
-        input_dir=Path("/data/qualiperf/P7-Perf/spt_results/simulation"),
-        xdmf_dir=xdmf_dir,
+        input_dir=input_dir,
+        xdmf_dir=output_dir,
         overwrite=False,
     )
     info: XDMFInfo = XDMFInfo.from_path(list(xdmf_paths.keys())[0])
@@ -37,22 +28,8 @@ if __name__ == "__main__":
         _, _ = mesh_datasets_from_xdmf(xdmf_path, overwrite=False)
 
 
-    # interpolate data
-    # Calculate tend time from all simulations
-    # tends: np.ndarray = np.zeros(shape=(len(list(xdmf_paths)),))
-    # for k, xdmf_path in enumerate(xdmf_paths):
-    #     xdmf_info = XDMFInfo.from_path(xdmf_path)
-    #     tends[k] = xdmf_info.tend
-    # tend = tends.min()
-    # for num in [10, 100]:
-    #     output_dir = xdmf_dir / f"{num}"
-    #     output_dir.mkdir(exist_ok=True, parents=True)
-    #
-    #     all_limits: List[DataLimits] = []
-    #     for xdmf_path in xdmf_paths:
-    #         interpolate_xdmf(
-    #             xdmf_in=xdmf_path,
-    #             xdmf_out=output_dir / f"{xdmf_path.stem}_interpolated.xdmf",
-    #             times_interpolate=np.linspace(0, tend, num=num),
-    #             overwrite=False,
-    #         )
+if __name__ == "__main__":
+
+    input_dir = Path("/data/qualiperf/P7-Perf/spt_results/simulation_2711")
+    output_dir = Path("/home/mkoenig/git/porous_media/data/spt/2023-12-05")
+    process_spt_simulations(input_dir=input_dir, output_dir=output_dir)
